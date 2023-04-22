3117c91077110f361398deabae05a719e656f9d5
maahdi boudaouara <mboudaouara@mercarue.com>
2023-04-22 20:55:36 +0100

    465

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-const { app } = require('./app');
-const http = require('http');
-require('dotenv').config()
+const { app } = require("./app");
+const http = require("http");
+require("dotenv").config();
 const server = http.createServer(app);
-const db=require('./config')
-db.connect(function(err) {
+const db = require("./config");
+db.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
 server.listen(
-    process.env.BID_PORT ,
+  process.env.BID_PORT,
+  process.env.BID_SERVER,
   console.log(`server is running at port http://${process.env.BID_SERVER}`)
 );
