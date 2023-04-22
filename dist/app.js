31a433e2cd6589a49a5af07eba0ebb5fa5befe75
maahdi boudaouara <mboudaouara@mercarue.com>
2023-04-22 18:37:37 +0100

    ffff

---
+++ dist/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const bodyparser = require('body-parser');
+app.use(cors());
+app.use(express.json());
+
+// body-parser middleware use
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+//import routes
+const bidRoute = require('./routes/bid');
+
+//route middlewares 
+app.use('/backend/bid', bidRoute);
+module.exports = {
+  app
+};
---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const {
+  app
+} = require('./app');
+const http = require('http');
+require('dotenv').config();
+const server = http.createServer(app);
+const db = require('./config');
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+server.listen(process.env.BID_PORT, console.log(`server is running at port http://${process.env.BID_SERVER}`));
